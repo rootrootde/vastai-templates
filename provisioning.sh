efbe0e0d5690bd0fcb1cbcafc6593a28e94ca88e
rootrootde <mail@rootroot.de>
2025-06-11 18:54:20 +0200

    Update provisioning script via GUI

--- provisioning.sh
+++ provisioning.sh
@@ -29,7 +29,7 @@ WORKFLOWS=(
 )
 
 CHECKPOINT_MODELS=(
-    "https://civitai.com/api/download/models/798204?type=Model&format=SafeTensor&size=full&fp=fp16"
+    "https://civitai.com/api/download/models/1759168"
 )
 
 UNET_MODELS=(
